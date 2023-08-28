159501086832fd9f4968691c884378331d60d08f
aler21620 <rod21620@uvg.edu.gt>
2023-08-27 21:09:32 -0600

    Displays
    Se cambio el uso de librería para los displays y se codifico dentro del main, el codigo funciona pero no todos los displays se mantienen encendidos

--- PROYECTO1-21620/src/main.cpp
+++ PROYECTO1-21620/src/main.cpp
@@ -9,7 +9,6 @@
 //**************************************************************************************************
 #include <Arduino.h>
 #include "driver/ledc.h"
-#include "display7.h"  //Crear librería para control de los displays
 #include "esp_adc_cal.h"
 #include "config.h" //Archivo de librería de Adafruit y el WIFI
 
@@ -37,27 +36,14 @@
 #define pinPWMS 2    // GPIO 2 para tener la salida del PWM del servo 
 #define toma_TEMP 25 //Botón para la toma de temperatura
 
-//Pinout de los displays
-#define pinDisplayA 27 //Pin A intercontado con multiplexeo para los 3 displays
-#define pinDisplayB 13 //Pin B intercontado con multiplexeo para los 3 displays
-#define pinDisplayC 12 //Pin C intercontado con multiplexeo para los 3 displays
-#define pinDisplayD 22 //Pin D intercontado con multiplexeo para los 3 displays
-#define pinDisplayE 4 //Pin E intercontado con multiplexeo para los 3 displays
-#define pinDisplayF 26 //Pin F intercontado con multiplexeo para los 3 displays
-#define pinDisplayG 21 //Pin G intercontado con multiplexeo para los 3 displays
-//El pin del punto no lo coloco, debido a que siempre estará encendido en el mismo display, por lo que lo conecté directamente a voltaje
-
-//Pinout del COM de los displays, conectados a los transistores
-#define display1 23
-#define display2 32
-#define display3 33
-
 //**************************************************************************************************
 // Prototipos de funciones
 //**************************************************************************************************
 uint32_t readADC_Cal(int ADC_Raw); //Función para leer el sensor de temperatura con ADC de ESP32
 void configurarPWM(void); //Función para configurar el PWM de las leds, el del servo está configurado en el setup
 void temperatura_led(void); //Función para indicar la led que tiene que encender según la temperatura
+void displays(void);  //Función para inicializar los displays y mostrar el valor 
+void valor_temperatura(void); //Función para arreglar el valor de temperatura y poder mostrarlo en los displays
 
 //**************************************************************************************************
 // Variables Globales
@@ -72,6 +58,11 @@ bool bandera = false;
 int decena; 
 int unidad; 
 int decimal; 
+
+int pinesDisplay[7] = {27, 13, 12, 22, 4, 26, 21}; //No coloco el pin del punto debido a que siempre estará encendido
+int pinesTransistores[3] = {23, 32, 33};
+bool displaysencendidos = false; 
+int valorestemperatura[4];
 
 //Variables para identificar la temperatura, según la tabla de proyecto 
 const float TEMP_LOW = 25.0;
@@ -94,16 +85,14 @@ int SERVO_HIGH = 135;
 void setup() {
   pinMode(toma_TEMP, INPUT_PULLUP);
   configurarPWM();
-  configdisplay7(pinDisplayA, pinDisplayB, pinDisplayC, pinDisplayD, pinDisplayE, pinDisplayF, pinDisplayG);
 
-  pinMode(display1, OUTPUT);
-  pinMode(display2, OUTPUT);
-  pinMode(display3, OUTPUT);
+  for (int i = 0; i < 7; i++) {
+    pinMode(pinesDisplay[i], OUTPUT);
+  }
 
-  //Configuración de los transistores del multiplexeo
-  digitalWrite(display1, LOW);
-  digitalWrite(display2, LOW);
-  digitalWrite(display3, LOW);
+  for (int i = 0; i < 3; i++) {
+    pinMode(pinesTransistores[i], OUTPUT);
+  }
 
   //Configuración del PWM para el servo motor
   // Paso 1: Configurar el módulo PWM
@@ -125,37 +114,25 @@ void setup() {
 //**************************************************************************************************
 void loop() {
   //io.run();
-
   botonpresionado = digitalRead(toma_TEMP);
   delay(100);
 
+  if (!displaysencendidos && botonpresionado == LOW) {
+    displaysencendidos = true;
+  }
+
   if (botonpresionado == LOW && botonanterior == HIGH) {
     bandera = true; 
+    if(displaysencendidos){
+      if (bandera) {
+        temperatura_led();
+        valor_temperatura();
+        displays();
+        delay(100);
+        bandera = false; 
+      }
+    }
   }
-
-  if (bandera) {
-    temperatura_led();
-    delay(100);
-    bandera = false; 
-  }
-
-  digitalWrite(display1, HIGH);
-  digitalWrite(display2, LOW);
-  digitalWrite(display3, LOW);
-  valor(0);
-  delay(100);
-/*
-  digitalWrite(display1, LOW);
-  digitalWrite(display2, HIGH);
-  digitalWrite(display3, LOW);
-  valor(1);
-  delay(100);
-
-  digitalWrite(display1, LOW);
-  digitalWrite(display2, LOW);
-  digitalWrite(display3, HIGH);
-  valor(2);
-  delay(100); */
 }
 
 //****************************************************************
@@ -222,4 +199,54 @@ void temperatura_led(void){
     ledcWrite(pwmChannel, map(SERVO_HIGH, 0, 180, 30, 115));
     Serial.print("LED Rojo encendido \n");
   }
+}
+
+//************************************************************************
+// Función para mostrar el valor de temperatura en los displays
+//************************************************************************
+void displays(){
+  //Esta variable, va a permitir tener todos los valores de los segmentos para mostrar en el display
+  //Por ser un display de cátodo, el 1 significa que se va a encender y el 0 que se va a apagar
+  int numeros[10][7] = {
+    {1, 1, 1, 1, 1, 1, 0}, // Número 0
+    {0, 1, 1, 0, 0, 0, 0}, // Número 1
+    {1, 1, 0, 1, 1, 0, 1}, // Número 2
+    {1, 1, 1, 1, 0, 0, 1}, // Número 3
+    {0, 1, 1, 0, 0, 1, 1}, // Número 4
+    {1, 0, 1, 1, 0, 1, 1}, // Número 5
+    {1, 0, 1, 1, 1, 1, 1}, // Número 6
+    {1, 1, 1, 0, 0, 0, 0}, // Número 7
+    {1, 1, 1, 1, 1, 1, 1}, // Número 8
+    {1, 1, 1, 1, 0, 1, 1}  // Número 9
+  };
+  
+  int estadopinestransistores[3][3] = {
+    {1, 0, 0}, // Esta sección habilita el display 1, en este caso el transistor 
+    {0, 1, 0}, // Esta sección habilita el display 2, en este caso el transistor 
+    {0, 0, 1}  // Esta sección habilita el display 3, en este caso el transistor 
+  };
+
+  for (int n = 0; n < 3; n++) {
+    for (int i = 0; i < 3; i++) {
+      digitalWrite(pinesTransistores[i], estadopinestransistores[n][i]);
+    }
+
+    for (int j = 0; j < 7; j++) {
+      digitalWrite(pinesDisplay[j], numeros[valorestemperatura[n]][j]);
+    }
+
+    delay(5); //Se coloca este delay para que no se perciba el cambio en los displays por el ojo humano
+  }
+}
+
+//************************************************************************
+//Función para arreglar el valor de la temperatura para los displays
+//************************************************************************
+void valor_temperatura(){
+  int temperatura = LM35_TempC_Sensor1 * 100; 
+
+  valorestemperatura[3] = ((temperatura) / 1) % 10;
+  valorestemperatura[2] = ((temperatura) / 10) % 10;
+  valorestemperatura[1] = ((temperatura) / 100) % 10;
+  valorestemperatura[0] = ((temperatura) / 1000) % 10;
 }
